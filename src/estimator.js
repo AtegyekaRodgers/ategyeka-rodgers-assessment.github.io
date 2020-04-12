abb7cc87570cb66a4e6c44e6189040c44b0d2d12
Rodgers <47475700+AtegyekaRodgers@users.noreply.github.com>
2020-04-12 07:04:24 +0300

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,66 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+	
+	/*data = {
+		region: {
+			name: "Africa",
+			avgAge: 19.7,
+			avgDailyIncomeInUSD: 5,
+			avgDailyIncomePopulation: 0.71
+		},
+		periodType: "days",
+		timeToElapse: 58,
+		reportedCases: 674,
+		population: 66622705,
+		totalHospitalBeds: 1380614
+	}; */
+	
+	let my_output = {
+		data: data,			// the input data I received
+		impact: {},			// my best case estimation
+		severeImpact: {}  // my severe case estimation 
+	}
+	const requestedTimeInDays = nomalizeTimeIntoDays(my_output.data.periodType, my_output.data.periodType.timeToElapse);
+	my_output.impact.currentlyInfected = my_output.data.reportedCases*10;
+	my_output.impact.infectionsByRequestedTime = my_output.impact.currentlyInfected*(2^(requestedTimeInDays/3));
+	my_output.impact.severeCasesByRequestedTime = 0.15*my_output.impact.infectionsByRequestedTime;
+	my_output.impact.hospitalBedsByRequestedTime = (0.35*my_output.data.totalHospitalBeds)-my_output.impact.severeCasesByRequestedTime;    
+	my_output.impact.casesForICUByRequestedTime = 0.05*my_output.impact.infectionsByRequestedTime;
+	my_output.impact.casesForVentilatorsByRequestedTime = 0.02*my_output.impact.infectionsByRequestedTime;
+	my_output.impact.dolarsInFlight = (my_output.data.region.avgDailyIncomePopulation*my_output.data.population)*(my_output.data.region.avgDailyIncomeInUSD*my_output.impact.infectionsByRequestedTime)*requestedTimeInDays;
+	//---------------------------
+	my_output.severeImpact.currentlyInfected = my_output.data.reportedCases*50;
+	my_output.severeImpact.infectionsByRequestedTime = my_output.severeImpact.currentlyInfected*(2^(requestedTimeInDays/3));         
+	my_output.severeImpact.severeCasesByRequestedTime = 0.15*my_output.severeImpact.infectionsByRequestedTime; 
+	my_output.severeImpact.hospitalBedsByRequestedTime = (0.35*my_output.data.totalHospitalBeds)-my_output.severeImpact.severeCasesByRequestedTime;    
+	my_output.severeImpact.casesForICUByRequestedTime = 0.05*my_output.severeImpact.infectionsByRequestedTime;
+	my_output.severeImpact.casesForVentilatorsByRequestedTime = 0.02*my_output.severeImpact.infectionsByRequestedTime;
+	my_output.severeImpact.dolarsInFlight = (my_output.data.region.avgDailyIncomePopulation*my_output.data.population)*(my_output.data.region.avgDailyIncomeInUSD*my_output.severeImpact.infectionsByRequestedTime)*requestedTimeInDays;      
+	return my_output;
+};
+
+const nomalizeTimeIntoDays = (periodType, timeToElapse) => {
+	switch (periodType){
+		case "days":
+			return timeToElapse; 
+		case "weeks":
+			return timeToElapse*7;
+		case "months":
+			return timeToElapse*30;
+		case "years":
+			return timeToElapse*365;
+		default:
+			return 1; 
+	}
+}
+ 
+
 
 export default covid19ImpactEstimator;
+
+
+
+
+
+
+
+
